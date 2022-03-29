4adcd873b08cbded5924ccd8e0921c524acb6108
chistyakov <langlydes@gmail.com>
2022-03-29 22:59:45 +0300

    initial

---
+++ src/main/VPT/Draw.java
@@ -0,0 +1,112 @@
+package VPT;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+public class Draw {
+
+    public static String convertInputStreamToString(InputStream is) throws IOException {
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        byte[] buffer = new byte[8192];
+        int length;
+        while((length = is.read(buffer)) != -1) {
+            result.write(buffer, 0, length);
+        }
+        return result.toString(StandardCharsets.UTF_8);
+    }
+
+    public static void writeToFile(String text, String filename) throws IOException {
+        BufferedWriter wr = new BufferedWriter(new FileWriter(filename,true));
+        wr.append('\n');
+        wr.append(text);
+        wr.close();
+    }
+
+
+    public static JFrame draw() throws IOException {
+        File file = new File("./file.txt");
+        try (InputStream in = new FileInputStream(file)) {
+        } catch (FileNotFoundException e) {
+            FileOutputStream of = new FileOutputStream(file, false);
+        }
+
+        JFrame f = new JFrame();
+
+        /* Поле ввода текста */
+        JTextField inputField = new JTextField();
+        inputField.setBounds(20,30,350,30);
+        f.add(inputField);
+
+        /* Поле вывода сообщений */
+        JTextArea outputArea = new JTextArea();
+        outputArea.setBounds(20,80,350,60);
+        outputArea.setEditable(false);
+        f.add(outputArea);
+
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setBounds(20,10,20,20);
+        f.add(fileChooser);
+
+        JButton inputButton = new JButton("Ввод");
+        inputButton.setBounds(20,200,100,30);
+        f.add(inputButton);
+        inputButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String inputText = inputField.getText();
+                outputArea.setText("");
+                InputStream is = null;
+                try {
+                    is = new FileInputStream(file);
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+                if (is == null) {
+                    outputArea.setText("File not found! Error.");
+                }
+                try {
+                    int gotLine = 0;
+                    assert is != null;
+                    String text = convertInputStreamToString(is);
+                    String[] textsplitted = text.split("\\r?\\n");
+                    for (String line: textsplitted) {
+                        if (Objects.equals(inputText, line)) {
+                            outputArea.setText("Text " + inputText + " already exists in the file.");
+                            gotLine = 1;
+                        }
+                    }
+                    if (gotLine != 1) {
+                        writeToFile(inputText, "./file.txt");
+                    }
+                } catch (IOException ex) {
+                    outputArea.setText("File is corrupted! Error.");
+                }
+            }
+        });
+
+        JButton clearButton = new JButton("Очистить");
+        clearButton.setBounds(140,200,100,30);
+        f.add(clearButton);
+        clearButton.addActionListener(e -> {
+            inputField.setText("");
+            outputArea.setText("");
+        });
+
+        JButton closeButton = new JButton("Закрыть");
+        closeButton.setBounds(260, 200,100,30);
+        f.add(closeButton);
+        closeButton.addActionListener(e -> System.exit(0));
+
+        f.setTitle("Тестовое приложение");
+        f.setSize(400,300);
+        f.setResizable(false);
+        f.setLayout(null);
+        f.setVisible(true);
+
+        return f;
+    }
+}
---
+++ src/main/VPT/Main.java
@@ -0,0 +1,11 @@
+package VPT;
+
+import javax.swing.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+       JFrame f =  Draw.draw();
+    }
+}
